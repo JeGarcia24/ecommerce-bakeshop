f207909bbb21e218d921084f488b50cf5566ec27
JeGarcia24 <jeralyn.garcia@yahoo.com>
2020-05-24 10:48:04 +0800

    Updates

---
+++ precache-manifest.300ba44d6dd5a9eb52c2dfd8c1f2b585.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7083c4985ce67d51ea31abd750ed7632",
+    "url": "/ecommerce-bakeshop/index.html"
+  },
+  {
+    "revision": "f2b1d5023f4a3cc3024c",
+    "url": "/ecommerce-bakeshop/static/css/main.8e4470a1.chunk.css"
+  },
+  {
+    "revision": "c94fa437ea85b9ba3dc4",
+    "url": "/ecommerce-bakeshop/static/js/2.ab494692.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecommerce-bakeshop/static/js/2.ab494692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b1d5023f4a3cc3024c",
+    "url": "/ecommerce-bakeshop/static/js/main.dee6ea14.chunk.js"
+  },
+  {
+    "revision": "af7a285f39f52a573103",
+    "url": "/ecommerce-bakeshop/static/js/runtime-main.dffc9750.js"
+  },
+  {
+    "revision": "9a8d8ee03aa45b002d905ff7efe590d9",
+    "url": "/ecommerce-bakeshop/static/media/about.9a8d8ee0.jpg"
+  },
+  {
+    "revision": "398a9a7e59d8b785730bff48dc645f70",
+    "url": "/ecommerce-bakeshop/static/media/bagels.398a9a7e.jpg"
+  },
+  {
+    "revision": "15e03ab130251a93d274217ed20d0529",
+    "url": "/ecommerce-bakeshop/static/media/blueberryCupcake.15e03ab1.jpg"
+  },
+  {
+    "revision": "ab2508489bec41b1e3aaa0625ab1b220",
+    "url": "/ecommerce-bakeshop/static/media/chocolateCake.ab250848.jpg"
+  },
+  {
+    "revision": "cf03f69eca1c9815a04d904227c894d7",
+    "url": "/ecommerce-bakeshop/static/media/chocolateChipCookie.cf03f69e.jpg"
+  },
+  {
+    "revision": "09340ef9aee7ac171031433303f3a65b",
+    "url": "/ecommerce-bakeshop/static/media/chocolateMuffin.09340ef9.jpg"
+  },
+  {
+    "revision": "a3923d171684c1c07decd1a5bb7cce51",
+    "url": "/ecommerce-bakeshop/static/media/cinnamonRoll.a3923d17.jpg"
+  },
+  {
+    "revision": "565c80c5e9db102bc40983c31984bfbd",
+    "url": "/ecommerce-bakeshop/static/media/croissant.565c80c5.jpg"
+  },
+  {
+    "revision": "d7091cb4bb1a5cf69543a26517d0f820",
+    "url": "/ecommerce-bakeshop/static/media/doughnuts.d7091cb4.jpg"
+  },
+  {
+    "revision": "23bbdd8998da7d8729e9c8ada07b478f",
+    "url": "/ecommerce-bakeshop/static/media/frenchBread.23bbdd89.jpg"
+  },
+  {
+    "revision": "0223fb75c98dd899e1db853bcd8e9a24",
+    "url": "/ecommerce-bakeshop/static/media/garlicBread.0223fb75.jpg"
+  },
+  {
+    "revision": "4299d1a57fec39eb3cca4d64bfd92dd6",
+    "url": "/ecommerce-bakeshop/static/media/header.4299d1a5.jpg"
+  },
+  {
+    "revision": "247ed70ff76a9bcb9be2eb8734b2d0a8",
+    "url": "/ecommerce-bakeshop/static/media/logo.247ed70f.jpg"
+  },
+  {
+    "revision": "cc4e0b9c2381868a0413b86202034fc9",
+    "url": "/ecommerce-bakeshop/static/media/redVelvetCupcake.cc4e0b9c.jpg"
+  },
+  {
+    "revision": "d61dbb2550eaa980b5d2400c436cfdfb",
+    "url": "/ecommerce-bakeshop/static/media/strawberryCake.d61dbb25.jpg"
+  },
+  {
+    "revision": "a017544601b8d5ec50a9ff97da84d1f4",
+    "url": "/ecommerce-bakeshop/static/media/strawberryCheesecake.a0175446.jpg"
+  },
+  {
+    "revision": "d2917d5cd0e4f33e889e64fd4960762a",
+    "url": "/ecommerce-bakeshop/static/media/strawberryPie.d2917d5c.jpg"
+  },
+  {
+    "revision": "b2dff3111e040649dee2c27c7e282747",
+    "url": "/ecommerce-bakeshop/static/media/toast.b2dff311.jpg"
+  },
+  {
+    "revision": "11f615084ae7b1f67c9f2c78ef66ff3f",
+    "url": "/ecommerce-bakeshop/static/media/wheatBread.11f61508.jpg"
+  }
+]);
